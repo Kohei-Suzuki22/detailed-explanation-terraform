d2be11deb3ebba30e051f3e8938427f2e33520e8
kohei <bhkbb0551@gmail.com>
2024-01-01 18:42:52 +0900

    terraform.workspaceを使った三項演算

--- backend_tutorial/ec2.tf
+++ backend_tutorial/ec2.tf
@@ -21,6 +21,6 @@ resource "aws_subnet" "vpc_sample_subnet_1a" {
 resource "aws_instance" "example" {
   subnet_id = aws_subnet.vpc_sample_subnet_1a.id
   ami = "ami-07c589821f2b353aa"
-  instance_type = "t2.micro"
+  instance_type = terraform.workspace == "default" ? "t2.medium" : "t2.micro"
   
 }
